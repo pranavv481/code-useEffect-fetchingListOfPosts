b1d6abf9ec3e8196de5c22bb84793c14cdadb1f8
pranavv481 <pranavv481@gmail.com>
2020-05-26 15:53:31 +0530

    fetching

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import DataFetching from './components/DataFetching';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DataFetching />
+      </div>
+    );
+  }
+}
+
+export default App;
